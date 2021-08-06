a002185af4c775a19a7ef68145a2cc374d33c854
RyukieSama <ryukie.sama@gmail.com>
2021-08-06 18:42:47 +0800

    init

---
+++ SwiftUIDemos/SwiftUIDemos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemos
+//
+//  Created by 王荣庆 on 2021/8/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemos/SwiftUIDemos/SwiftUIDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemosApp.swift
+//  SwiftUIDemos
+//
+//  Created by 王荣庆 on 2021/8/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
